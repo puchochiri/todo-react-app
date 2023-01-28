9a6efd8ed461d3fa4dd961d54cdc6e5325ad99a5
양선우 <doawishfor@naver.com>
2023-01-28 21:53:04 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import './App.css';
+import Todo from './Todo';
+import React, {useState} from 'react';
+import {List, Paper} from "@mui/material"
+
+function App() {
+  const [items, setItem] = useState([
+  {
+    id: "0",
+    title: "Hello World 1",
+    done: true,
+  },
+  {
+    id: "1",
+    title: "Hello World 2",
+    done: true,
+  },
+]);
+
+let todoItems =
+  items.length > 0 && (
+    <Paper style={{ margin: 16 }}>
+      <List>
+        {items.map((item) => (
+          <Todo item={item} key={item.id} />
+        ))}
+      </List>
+
+    </Paper>
+  )
+
+
+  return (
+    <div className="App">
+      {todoItems}
+    </div>
+  );
+  
+}
+
+export default App;
---
+++ src/Todo.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { ListItem, ListItemText, InputBase, Checkbox }  from "@mui/material";
+
+const Todo = (props) => {
+
+    const [item, setItem] = useState(props.item);
+    return (
+        <ListItem>
+            <Checkbox checked={item.done} />
+            <ListItemText>
+                <InputBase
+                    inputProps={{ "aria-label": "naked" }}
+                    type="text"
+                    id={item.id}
+                    name={item.id}
+                    value={item.title}
+                    multiline={true}
+                    fullwidth={true}
+                 />
+            </ListItemText>
+        </ListItem>
+    );
+};
+
+export default Todo;
+
+
+
+
