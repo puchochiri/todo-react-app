acbbe770bcd76b843a05f9d2a8dc691c35fbab81
puchochiri <doawishfor@naver.com>
2023-03-01 19:04:26 +0900

    commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
+import AppRouter from './AppRouter';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+
+const root = createRoot(container);
+
+root.render(<AppRouter tab="home" />);
+
+reportWebVitals();
+
+
+
+/*
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
