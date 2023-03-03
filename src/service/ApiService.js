15b45600e02dcf27f3a1861df5cff6b73b84321b
puchochiri <doawishfor@naver.com>
2023-03-03 19:20:23 +0900

    commit

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,10 +1,19 @@
 import { API_BASE_URL } from "../app-config";
 
 export function call(api, method, request) {
+  let headers= new Headers({
+    "Content-Type": "application/json",
+  });
+
+  //로컬 스토리지에서 ACCESS TOKEN 가져오기
+  const accessToken = localStorage.getItem("ACCESS_TOKEN");
+  if(accessToken && accessToken !== null){
+    headers.append("Authorization", "Bearer " + accessToken);
+  }
+
+
   let options = {
-    headers: new Headers({
-      "Content-Type": "application/json",
-    }),
+    headers: headers,
     url: API_BASE_URL + api,
     method: method,
   };
@@ -18,8 +27,7 @@ export function call(api, method, request) {
     } else if(response.status === 403) {
       window.location.href = "/login"; // redirect
     } else {
-      Promise.reject(response);
-      throw Error(response);
+      new Error(response);
     }
   }).catch((error) => {
     console.log("http error");
@@ -33,7 +41,23 @@ export function call(api, method, request) {
 export function signin(userDTO) {
   return call("/auth/signin", "POST", userDTO)
     .then((response) => {
-      console.log("response : ", response);
-      alert("로그이 토큰: " + response.token);
-    })
+      //console.log("response : ", response);
+      //alert("로그이 토큰: " + response.token);
+      if(response.token){
+        // 로컬 스토리지에 토큰 저장
+        localStorage.setItem("ACCESS_TOKEN", response.token);
+        //token에 존재하는 경우 Todo 화면으로 리디렉트
+        window.location.href = "/";
+      }
+    });
+}
+
+
+export function signout() {
+  localStorage.setItem("ACCESS_TOKEN", null);
+  window.location.href = "/login";
+}
+
+export function signup(userDTO) {
+  return call("/auth/signup","POST",userDTO);
 }
